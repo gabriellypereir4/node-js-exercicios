6d8030cb88deff0bc070b83b3c138f3d5f53343e
Gabrielly <gabriellydiaspereira15@gmail.com>
2024-09-24 21:43:39 -0300

    exemplo

---
+++ exercicios/basico/exemplo.js
@@ -0,0 +1,6 @@
+// Importando o pacote
+let prompt = require('prompt-sync')()
+// Usando o pacote para receber um valor do usuário
+let name = prompt('Qual seu nome? ')
+// Usamos o + para juntar uma string(texto) com uma variável
+console.log('Oi ' + name)
